d07c84cc0e678cc03ceb9e5b5178de8ef4df774c
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-06-08 17:06:11 -0300

    On branch SO_Job  Changes to be committed: 	modified:   job.py                     movido de um diretorio pra outro

--- PSE/SO/job.py
+++ PSE/SO/job.py
@@ -1,5 +1,24 @@
 class Job(object):
     """docstring for Job"""
-    def __init__(self, Ti, Tf, ):
+    def __init__(self, Ti, Ttotal, tamMem, IOcount):
         super(Job, self).__init__()
-        self.arg = arg
+
+        if Ti >= 0:
+            self._Ti = Ti
+        else:
+            self._Ti = 0
+
+        if Ttotal >= 0:
+            self.Ttotal = Ttotal
+        else:
+            self._Ttotal = 0
+
+        if tamMem >= 0:
+            self.tamMem = tamMem
+        else:
+            self._tamMem = 0
+
+        if IOcount >= 0:
+            self.IOcount = IOcount
+        else:
+            self._IOcount = 0
