945d839705bd0eaafc44de2bfa4bda0f08af082f
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-07-22 18:12:35 -0300

    oi11

--- PSE/SO/job.py
+++ PSE/SO/job.py
@@ -3,7 +3,7 @@ from PSE.SO import Mensagem, Evento
 
 class Job(object):
     """docstring for Job"""
-    def __init__(self, nome, T_chegada, T_MaxCPU, segmentos, DiscoCount, LeitoraCount, ImpressoraCount):
+    def __init__(self, nome, T_chegada, T_MaxCPU, segmentos, DiscoCount, arquivos, LeitoraCount, ImpressoraCount):
         super(Job, self).__init__()
 
         self.nome = nome
@@ -13,35 +13,42 @@ class Job(object):
         self.tempo_transcorrido = 0
         self.segmentos = segmentos
         self.segmentos_ativos = set()
-        self.DiscoCount = DiscoCount  # qtde de acessos ao disco
-        self.LeitoraCount = LeitoraCount  # qtde de leituras de cartao
+        self.arquivos = arquivos
+        self.DiscoCount = DiscoCount        # qtde de acessos ao disco
+        self.LeitoraCount = LeitoraCount    # qtde de leituras de cartao
         self.ImpressoraCount = ImpressoraCount   # qtde de impressoes
 
         random.seed(T_MaxCPU + ImpressoraCount + LeitoraCount)
         # programa uma serie de eventos
         self.eventos_programados = list()
 
-        qtde_eventos = ImpressoraCount + LeitoraCount #+ len(segmentos)
+        qtde_eventos = ImpressoraCount + LeitoraCount + DiscoCount#+ len(segmentos)
 
         times = [i*T_MaxCPU/(qtde_eventos + 1) for i in range(1, qtde_eventos + 1)]
-        m = map(lambda e : e + random.gauss(0.0, T_MaxCPU*.05), times)
+        m = map(lambda e : e + random.gauss(0.0, .065*T_MaxCPU/(qtde_eventos + 1)), times)
         times = [int(e) for e in m]
         # embaralha os instantes de interrupcao
         random.shuffle(times)
+        print(times)
 
-        # programa as mudancas de segmento
+        ## programa as mudancas de segmento
         # for _ in range(len(self.segmentos)):
         #     novo_evento = Evento('<RequisitarMemoria>', times.pop(), self)
         #     self.eventos_programados.append(novo_evento)
         # embaralha a ordem em que os segmentos sao chamados
-        random.shuffle(self.segmentos)
+        # random.shuffle(self.segmentos)
 
-        # programa as impressoes
+        ## programa os acessos a arquivo (disco)
+        for _ in range(self.DiscoCount):
+            eventoAcessoDisco = Evento('<AcessarArquivo>', times.pop(), self, random.choice(self.arquivos))
+            self.eventos_programados.append(eventoAcessoDisco)
+
+        ## programa as impressoes
         for _ in range(self.ImpressoraCount):
             eventoImpressao = Evento('<Imprimir>', times.pop(), self, random.choice(['P1', 'P2']))
-            self.eventos_programados.append([eventoImpressao, False])
+            self.eventos_programados.append(eventoImpressao)
 
-        # programa as leituras
+        ## programa as leituras
         for _ in range(self.LeitoraCount):
             eventoLeitura = Evento('<Leitura>', times.pop(), self, random.choice(['L1', 'L2']))
             self.eventos_programados.append(eventoLeitura)
@@ -73,7 +80,7 @@ class Job(object):
         self._sinc(tempo_avanco)
         raise Mensagem('time slice completado')
 
-    def gera_log(self):
+    def log_job(self):
         print('Job:', self.nome)
         print('\tinstante de chegada:', self.T_chegada)
         print('\ttempo maximo de CPU:', self.T_MaxCPU)
@@ -83,17 +90,18 @@ class Job(object):
         print('\tQuantidade de acessos aa Impressora:', self.ImpressoraCount)
         print('\tEventos programados (tempos a partir do inicio da execucao do processo):')
         if len(self.eventos_programados) > 0:
-            for evento in sorted(self.eventos_programados, key = lambda t : t[0].T_ocorrencia):
-                print('\t\ttipo: {0}\tinstante previsto: {1}'.format(evento[0].tipo, evento[0].T_ocorrencia))
+            for evento in sorted(self.eventos_programados, key = lambda t : t.T_ocorrencia):
+                print('\t    ', evento)
         print('\tTempo de espera em fila de CPU:', self.tempo_espera_CPU)
         print('\tTempo de espera em fila de Memoria:', self.tempo_espera_Memoria)
         print('\tTempo de espera em fila de Impressoras:', self.tempo_espera_Impressoras)
         print('\tTempo de espera em fila de Leitoras:', self.tempo_espera_Leitoras)
 
     def prox_segmento(self):
-        seg = self.segmentos.pop()
-        self.segmentos.insert(0, seg)
-        return seg
+        return random.choice(self.segmentos)
+
+    # def prox_arquivo(self):
+    #     return random.choice(self.arquivos)
 
     def __eq__(self, job):
         if isinstance(job, Job):
