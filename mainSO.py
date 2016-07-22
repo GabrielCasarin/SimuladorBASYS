f8d8203fc4ede7da6dd3cecd7e0fc394f3d4bf90
Gabriel Casarin da Silva <gabriel.casarin@hotmail.com>
2016-07-22 18:12:27 -0300

    mudado o formato do arquivo de jobs

--- mainSO.py
+++ mainSO.py
@@ -1,73 +1,58 @@
 from PSE.SO import Job, Maquina, Evento
 from PSE.Base import Simulador
-import re
+import json
+# import re
 
-## Le os parametro globais da simulacao
-with open('parametros_simulador.txt') as conf_sim:
-    linhas = conf_sim.readlines()
-    for i in range(len(linhas)):
-        linhas[i]= re.split(r'\s*=\s*', linhas[i][:-1])
-    for atributo in linhas:
-        if atributo[0] == 'time_slice_size':
-          time_slice_size = int(atributo[1])
-        elif atributo[0] == 'max_processos':
-          max_processos = int(atributo[1])
-        elif atributo[0] == 'disco_tempo_leitura':
-          disco_tempo_leitura = int(atributo[1])
-        elif atributo[0] == 'disco_tempo_escrita':
-          disco_tempo_escrita = int(atributo[1])
-        elif atributo[0] == 'disco_tamanho':
-          disco_tamanho = int(atributo[1])
-        elif atributo[0] == 'memoria_tempo_relocacao':
-          memoria_tempo_relocacao = int(atributo[1])
-        elif atributo[0] == 'memoria_tempo_transferencia':
-          memoria_tempo_transferencia = int(atributo[1])
-        elif atributo[0] == 'memoria_tamanho':
-          memoria_tamanho = int(atributo[1])
-        elif atributo[0] == 'impressora_tempo_impressao':
-          impressora_tempo_impressao = int(atributo[1])
-        elif atributo[0] == 'leitora_tempo_leitura':
-          leitora_tempo_leitura = int(atributo[1])
+def main():
+    ## Le os parametro globais da simulacao
+    with open('parametros_simulador.json') as conf_sim:
+        parametros = json.load(conf_sim)
 
+    with open('arquivos.json') as arquivos_conf:
+        arquivos_conf_dict = json.load(arquivos_conf)
 
-with open('so1.txt') as arq_jobs:
-  linhas = arq_jobs.readlines()
-  # determina o tempo de inicio do clock
-  T_acionamento_clk = int(linhas[0])
-  # e o de termino da simulacao
-  T_final = int(linhas[1])
+    # nome_arq_jobs = input('Digite o nome do arquivo de especificacao dos jobs: ')
+    nome_arq_jobs = 'jobs1.json'
+    with open(nome_arq_jobs) as arq_jobs:
+      jobs_conf = json.load(arq_jobs)
 
-  # carrega todos os jobs que serao simulados
-  padrao = re.compile(r'\((\w),([0-9]+)\)')
-  jobs = list()
-  for linha in linhas[2:]:  # pula as duas primeiras linhas do arquivo de jobs
-      linha = linha.split()
-      nome = linha[0] # identificador do job
-      ti = int(linha[1]) # tempo de chegada
-      tmax = int(linha[2]) # tempo maximo de cpu
-      estrutura_seg = list() # estrutura de segmentos do tipo (nome, tamanho)
-      seg_iter = padrao.finditer(linha[3])
-      for seg in seg_iter:
-          estrutura_seg.append((seg.group(1),int(seg.group(2))))
-      ioqtde = int(linha[4]) # quantidade de acessos ao disco
-      impressao_qtde = int(linha[5])
-      leitura_qtde = int(linha[6])
+      # determina o tempo de inicio do clock
+      T_acionamento_clk = jobs_conf['T_acionamento_clk']
+      # e o de termino da simulacao
+      T_final = jobs_conf['T_final_clk']
 
-      # cria um novo job
-      novo_job = Job(nome, T_chegada=ti, T_MaxCPU=tmax, segmentos=estrutura_seg, DiscoCount=0, LeitoraCount=leitura_qtde, ImpressoraCount=impressao_qtde)
-      jobs.append(novo_job)
+      # carrega todos os jobs que serao simulados
+      jobs = jobs_conf['jobs']
+      for job in jobs:
 
-mac = Maquina(T_acionamento_clk, T_final, time_slice_size=time_slice_size, max_processos=max_processos, disco_tempo_leitura=disco_tempo_leitura, disco_tempo_escrita=disco_tempo_escrita, disco_tamanho=disco_tamanho, memoria_tempo_relocacao=memoria_tempo_relocacao, memoria_tempo_transferencia=memoria_tempo_transferencia, memoria_tamanho=memoria_tamanho, leitora_tempo_leitura=leitora_tempo_leitura, impressora_tempo_impressao=impressora_tempo_impressao)#, jobs=jobs)
-sim = Simulador(mac)
-for job in jobs:
-  eventoChegadaJob = Evento('<Iniciar>', job.T_chegada, job)
-  sim.addTask(eventoChegadaJob, 1, eventoChegadaJob.T_ocorrencia)
-sim.simulate()
+          # cria um novo job
+          novo_job = Job(nome=job['nome'],      # identificador do job
+                        T_chegada=job['Ti'],    # tempo de chegada
+                        T_MaxCPU=job['Tmax'],       # tempo maximo de cpu
+                        segmentos=job['segmentos'], # estrutura de segmentos do tipo (nome, tamanho)
+                        arquivos=job['arquivos'],
+                        DiscoCount=job['IO'],              # quantidade de acessos ao disco
+                        ImpressoraCount=job['impressoes'], # quantidade de impressoes
+                        LeitoraCount=job['leituras']       # quantidade de leituras
+                     )
+          novo_job.log_job()
 
-print('\n')
-print('################')
-print('#     Jobs     #')
-print('################')
-for job in jobs:
-    job.gera_log()
-    print()
+    #       jobs.append(novo_job)
+
+    # mac = Maquina(T_acionamento_clk, T_final, **parametros)#, jobs=jobs)
+    # sim = Simulador(mac)
+    # for job in jobs:
+    #   eventoChegadaJob = Evento('<Iniciar>', job.T_chegada, job)
+    #   sim.addTask(eventoChegadaJob, 1, eventoChegadaJob.T_ocorrencia)
+    # sim.simulate()
+    #
+    # print('\n')
+    # print('################')
+    # print('#     Jobs     #')
+    # print('################')
+    # for job in jobs:
+    #     job.log_job()
+    #     print()
+
+if __name__ == '__main__':
+    main()
